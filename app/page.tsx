5f7fdf9bbf376d46adc177b8b2d32289e2b5397e
witcradg <witcradg@gmail.com>
2023-12-30 14:12:34 -0800

    Shadcn UI Crash Course #2 - Card Components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,53 @@
-import Image from 'next/image'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
-export default function Home() {
+interface Recipe {
+  title: string,
+  image: string,
+  time: number,
+  description: string,
+  vegan: boolean,
+  id: string
+}
+
+// json-server --watch ./_data/db.json --port 4000
+async function getRecipes(): Promise<Recipe[]> {
+  const result = await fetch('http://localhost:4000/recipes')
+  return result.json()
+}
+
+export default async function Home() {
+  const recipes = await getRecipes()
+
   return (
     <main>
-      home
+      <div className="grid grid-cols-3 gap-8">
+        {/* card component first look https://youtu.be/sXrwh4I229Q?t=202 */}
+        {recipes.map((recipe) => (
+          <Card key={recipe.id} className="flex flex-col justify-between">
+            <CardHeader className="flex-row gap-4 items-center">
+              {/* avatar */}
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook.</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <button>View Recipe</button>
+              {recipe.vegan && <p>Vegan!</p>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </main>
   )
 }
